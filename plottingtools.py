1cf5b41d17dcb3056a1172a8d383c9bc3b28bc71
Ma-Fi-94 <33027321+Ma-Fi-94@users.noreply.github.com>
2021-12-30 15:02:37 +0100

    Add files via upload

---
+++ plottingtools.py
@@ -0,0 +1,479 @@
+import matplotlib  # type: ignore
+import matplotlib.pyplot as plt  # type: ignore
+import matplotlib.figure  # type: ignore
+from matplotlib.patches import Rectangle  # type: ignore
+import seaborn as sns  # type: ignore
+import numpy as np
+import sys
+import types
+
+from typing import Tuple
+from typing import List
+
+####################
+# Internal helpers #
+####################
+
+
+def _set_fgbg(fg: str, bg: str):
+    ''' Internal helper to change fore- and background colours '''
+    plt.rcParams.update({
+        "lines.color": fg,
+        "patch.edgecolor": fg,
+        "text.color": fg,
+        "axes.facecolor": bg,
+        "axes.edgecolor": fg,
+        "axes.labelcolor": fg,
+        "xtick.color": fg,
+        "ytick.color": fg,
+        "grid.color": fg,
+        "figure.facecolor": bg,
+        "figure.edgecolor": bg,
+        "savefig.facecolor": bg,
+        "savefig.edgecolor": bg
+    })
+
+
+def _similarity_matrix(list_of_lists: List[List[float]],
+                       method: str = "jaccard"):
+    if type(method) == str:
+        if method == "jaccard":
+            f = lambda s1, s2: len(set.intersection(s1, s2)) / len(
+                set.union(s1, s2))
+        else:
+            raise NotImplementedError
+    elif type(method) is types.LambdaType:
+        f = method
+    else:
+        raise NotImplementedError
+
+    results = np.zeros((len(list_of_lists), len(list_of_lists)))
+
+    list_of_sets = list(map(lambda l: set(l), list_of_lists))
+
+    # Some similarity functions are not symmetric
+    # Hence we iterate over all n x n combinations
+    for i, set1 in enumerate(list_of_sets):
+        for j, set2 in enumerate(list_of_sets):
+            results[i, j] = f(set1, set2)
+    return results
+
+
+#################
+# Colour Themes #
+#################
+
+
+def darkmode(foreground="0.85", background="0.15"):
+    ''' Switch to darkmode. Foreground and background colours may also be specified explicitly. '''
+    _set_fgbg(fg=foreground, bg=background)
+
+
+def lightmode(foreground="0", background="1.0"):
+    ''' Switch to lightmode. Foreground and background colours may also be specified explicitly. '''
+    _set_fgbg(fg=foreground, bg=background)
+
+
+####################
+# Types of layouts #
+####################
+
+
+def singleplot(size=(10, 7)) -> Tuple[matplotlib.figure.Figure, plt.Axes]:
+    ''' Make a new 10x7 plot. Size can also be changed. '''
+    assert type(size) in (tuple, list)
+    assert len(size) == 2
+    w, h = size
+    assert type(w) in [int, float]
+    assert type(h) in [int, float]
+
+    fig, ax = plt.subplots(1, 1, figsize=size)
+    return fig, ax
+
+
+def multiplot(
+        nrows: int, ncols: int,
+        size_xy: Tuple[float,
+                       float]) -> Tuple[matplotlib.figure.Figure, plt.Axes]:
+    assert type(nrows) == int
+    assert type(ncols) == int
+    assert nrows > 0
+    assert ncols > 0
+    l, h = size_xy
+    assert type(l) in [float, int]
+    assert type(h) in [float, int]
+    assert l > 0
+    assert h > 0
+
+    fig, ax = plt.subplots(nrows, ncols, figsize=(h, l))
+    return fig, ax
+
+
+#############################
+# Adding elements to a plot #
+#############################
+
+
+def title(ax: plt.Axes,
+          title: str,
+          fontsize: float = 40,
+          pad: float = 20) -> None:
+    ''' Add a title to a plot. '''
+    assert type(title) == str
+    assert type(fontsize) in [int, float]
+    assert type(pad) in [int, float]
+    assert fontsize > 0
+    ax.set_title(title, fontsize=fontsize, pad=pad)
+
+
+def labels(ax: plt.Axes,
+           xlabel: str = None,
+           ylabel: str = None,
+           fontsize: float = 30,
+           pad: float = 15) -> None:
+    ''' Add axes labels to a plot. '''
+    assert type(fontsize) in [float, int]
+    assert fontsize > 0
+    assert type(pad) in [float, int]
+
+    if xlabel is not None:
+        assert type(xlabel) == str
+        ax.set_xlabel(xlabel, fontsize=fontsize, labelpad=pad)
+
+    if ylabel is not None:
+        assert type(ylabel) == str
+        ax.set_ylabel(ylabel, fontsize=fontsize, labelpad=pad)
+
+
+def diagonal(ax: plt.Axes,
+             colour: str = "black",
+             alpha=0.3,
+             linestyle="-",
+             linewidth=2):
+    ''' Add the 45 degrees diagonal to a plot. '''
+    assert type(colour) == str
+    assert type(alpha) in [float, int]
+    assert (alpha >= 0) and (alpha <= 1)
+    assert linestyle in ["-", "--", "-.", ":"]
+    assert type(linewidth) in [float, int]
+    assert linewidth > 0
+
+    minimum = min(ax.get_xlim()[0], ax.get_ylim()[0])
+    maximum = max(ax.get_xlim()[1], ax.get_ylim()[1])
+    ax.plot([minimum, maximum], [minimum, maximum],
+            color=colour,
+            linestyle=linestyle,
+            linewidth=linewidth,
+            alpha=alpha)
+
+
+def rectangle(ax: plt.Axes,
+              x1: float,
+              y1: float,
+              x2: float,
+              y2: float,
+              colour: str = "red",
+              linewidth: float = 3,
+              linestyle: str = "-",
+              fill: bool = False,
+              **kwargs):
+    ''' Add a rectangle to a plot. '''
+    assert type(x1) in [int, float]
+    assert type(y1) in [int, float]
+    assert type(x2) in [int, float]
+    assert type(y2) in [int, float]
+    assert type(colour) == str
+    assert type(linewidth) in [int, float]
+    assert linewidth >= 0
+    assert linestyle in ["-", "--", "-.", ":"]
+    assert type(fill) == bool
+
+    ax.add_patch(
+        Rectangle((x1, y1),
+                  x2 - x1,
+                  y2 - y1,
+                  linewidth=linewidth,
+                  linestyle=linestyle,
+                  edgecolor=colour,
+                  facecolor=colour,
+                  fill=fill,
+                  **kwargs))
+
+
+def star(ax: plt.Axes,
+         x: float,
+         y: float,
+         colour: str = "red",
+         fontsize: float = 50):
+    assert type(x) in [int, float]
+    assert type(y) in [int, float]
+    assert type(fontsize) in [int, float]
+    assert type(colour) == str
+
+    ax.annotate("*", (x, y), c=colour, fontsize=fontsize)
+
+
+def lines(ax: plt.Axes,
+          which: str,
+          pos: List[float],
+          colour: str = "black",
+          alpha: float = 0.3,
+          linestyle: str = "-",
+          linewidth: float = 2,
+          zorder: float = -100):
+    assert type(which) == str
+    assert which in ["x", "y"]
+
+    assert type(pos) == list
+    assert len(pos) > 0
+    for x in pos:
+        assert type(x) in [int, float]
+
+    assert type(colour) == str
+
+    assert type(alpha) in [int, float]
+    assert alpha > 0
+    assert alpha <= 1
+
+    assert type(linestyle) == str
+    assert linestyle in ["-", ":", "--", "-."]
+
+    assert type(linewidth) in [int, float]
+    assert linewidth > 0
+
+    assert type(zorder) in [int, float]
+
+    if which == "x":
+        ymin, ymax = ax.get_ylim()
+        for x in pos:
+            ax.vlines([x],
+                      ymin,
+                      ymax,
+                      color=colour,
+                      alpha=alpha,
+                      linestyle=linestyle,
+                      linewidth=linewidth,
+                      zorder=zorder)
+    elif which == "y":
+        xmin, xmax = ax.get_xlim()
+        for y in pos:
+            ax.hlines([y],
+                      xmin,
+                      xmax,
+                      color=colour,
+                      alpha=alpha,
+                      linestyle=linestyle,
+                      linewidth=linewidth,
+                      zorder=zorder)
+
+
+#############################
+# Change elements of a plot #
+#############################
+
+
+def despine(ax: plt.Axes, which: List[str] = ['top', 'right']):
+    ''' Remove spines of ax object. Spines can be specified, default is top and right. '''
+    assert type(which) == list
+    for spine in which:
+        assert spine in ['top', 'right', 'left', 'bottom']
+        ax.spines[spine].set_visible(False)
+
+
+def ticklabelsize(ax: plt.Axes, which: str = "both", size: float = 20):
+    ''' Change ticklabelsize of an ax object. '''
+    assert type(which) == str
+    assert type(size) in [int, float]
+    ax.tick_params(which, labelsize=size)
+
+
+def limits(ax: plt.Axes,
+           xlimits: Tuple[float, float] = None,
+           ylimits: Tuple[float, float] = None):
+    ''' Set axes limits of ax object. '''
+    if xlimits is not None:
+        assert type(xlimits) in (tuple, list)
+        assert len(xlimits) == 2
+        lo, hi = xlimits
+        #assert type(lo) in [int, float, np.float64] TBD
+        #assert type(hi) in [int, float, np.float64]
+        ax.set_xlim(xlimits)
+
+    if ylimits is not None:
+        assert type(ylimits) in (tuple, list)
+        assert len(ylimits) == 2
+        lo, hi = ylimits
+        #assert type(lo) in [int, float, np.float64]
+        #assert type(hi) in [int, float, np.float64]
+        ax.set_ylim(ylimits)
+
+
+def ticks_and_labels(ax: plt.Axes,
+                     which: str,
+                     ticks: List[float],
+                     labels=None):
+    assert which in ["x", "y", "xy", "yx"]
+    assert type(ticks) == list
+    for tick in ticks:
+        assert type(tick) in [float, int]
+
+    if labels is None:
+        labels = list(map(lambda x: str(x), ticks))
+    else:
+        assert type(labels) == list
+        assert len(ticks) == len(labels)
+
+    if which == "x" or which == "xy" or which == "yx":
+        ax.set_xticks(ticks)
+        ax.set_xticklabels(labels)
+    if which == "y" or which == "xy" or which == "yx":
+        ax.set_yticks(ticks)
+        ax.set_yticklabels(labels)
+
+
+def rotate_ticklabels(ax: plt.Axes, which: str, rotation: float):
+    assert type(which) == str
+    assert which in ["x", "y", "xy", "yx", "both"]
+    assert type(rotation) in [float, int]
+
+    if which == "x" or which == "xy" or which == "yx" or which == "both":
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=rotation)
+    if which == "y" or which == "xy" or which == "yx" or which == "both":
+        ax.set_yticklabels(ax.get_yticklabels(), rotation=rotation)
+
+
+def align_ticklabels(ax: plt.Axes, which: str, horizontal: str, vertical: str):
+    assert type(which) == str
+    assert which in ["x", "y"]
+
+    if which == "x":
+        if horizontal is not None:
+            assert type(horizontal) == str
+            assert horizontal in ['center', 'right', 'left']
+            ax.set_xticklabels(ax.get_xticklabels(),
+                               horizontalalignment=horizontal)
+        if vertical is not None:
+            assert type(vertical) == str
+            assert vertical in ['center', 'top', 'bottom', 'baseline']
+            ax.set_xticklabels(ax.get_xticklabels(),
+                               verticalalignment=vertical)
+
+    if which == "y":
+        if horizontal is not None:
+            assert type(horizontal) == str
+            assert horizontal in ['center', 'right', 'left']
+            ax.set_yticklabels(ax.get_yticklabels(),
+                               horizontalalignment=horizontal)
+        if vertical is not None:
+            assert type(vertical) == str
+            assert vertical in ['center', 'top', 'bottom', 'baseline']
+            ax.set_yticklabels(ax.get_yticklabels(),
+                               verticalalignment=vertical)
+
+
+#########
+# Plots #
+#########
+
+
+def similarity_heatmap(ax: plt.Axes,
+                       list_of_lists: List[List[float]],
+                       method: str = "jaccard"):
+    sns.heatmap(_similarity_matrix(list_of_lists, method))
+
+
+def masked_heatmap(ax: plt.Axes, data: np.ndarray, mask: str, **kwargs):
+    assert type(data) == np.ndarray
+
+    assert type(mask) == str
+    assert mask in ["lower", "upper", "lowerdiag", "upperdiag"]
+
+    if mask == "upperdiag":
+        mask_array = np.triu(data, k=0)
+    elif mask == "upper":
+        mask_array = np.triu(data, k=1)
+    elif mask == "lowerdiag":
+        mask_array = np.tril(data, k=0)
+    elif mask == "lower":
+        mask_array = np.tril(data, k=-1)
+
+    sns.heatmap(data, ax=ax, mask=mask_array)
+
+
+##################
+# Export figures #
+##################
+
+
+def save_png(filename: str, dpi: int = 300):
+    ''' Save current figure as png file. DPI can be specified. '''
+    assert type(filename) == str
+    assert filename != ""
+    assert type(dpi) in [int, float]
+    assert dpi > 0
+    plt.savefig(filename, dpi=dpi, bbox_inches="tight", format="png")
+
+
+def save_svg(filename: str):
+    ''' Save current figure as svg file. '''
+    assert type(filename) == str
+    assert filename != ""
+    plt.savefig(filename, bbox_inches="tight", format="svg")
+
+
+######################
+# Default parameters #
+######################
+
+
+def heatmap_cbar_kws():
+    return {'fraction': 0.1, 'shrink': 0.4, 'aspect': 7, 'pad': 0.05}
+
+
+def heatmap_annot_kws():
+    return {'fontsize': 20}
+
+
+
+
+##########################
+# Some ideas, TBD nicely #
+##########################
+
+def majorline(ax, x, y, linewidth=3, linestyle="-", **kwargs):
+	ax.plot(x, y, lw=linewidth, ls=linestyle, ** kwargs)
+
+def midiline(ax, x, y, linewidth=2, linestyle="--", **kwargs):
+	ax.plot(x, y, lw=linewidth, ls=linestyle, ** kwargs)
+	
+def minorline(ax, x, y, linewidth=1, linestyle=":", **kwargs):
+	ax.plot(x, y, lw=linewidth, ls=linestyle, ** kwargs)
+
+def legend(ax, loc="best", fontsize=25, frame=False, **kwargs):
+	''' Add a legend to a plot. '''
+	l = ax.legend(loc=loc, fontsize=fontsize, frameon=frame, **kwargs)
+	try:
+		for lh in l.legendHandles: 
+			lh._legmarker.set_alpha(1)
+	except:
+		pass
+	try:
+		for lh in l.legendHandles: 
+			lh.set_alpha(1)
+	except:
+		pass
+
+
+def oligoscatter(ax, x, y, marker="o", alpha=1, **kwargs):
+	ax.scatter(x, y, marker=marker, alpha=alpha, **kwargs)
+
+def oligoscatter_errorbar(ax, x, y, xerr=None, yerr=None, marker="o", alpha=1, **kwargs):
+	ax.errorbar(x, y, xerr=xerr, yerr=yerr, marker=marker, alpha=alpha, **kwargs)
+
+def polyscatter(ax, x, y, marker=".", alpha=0.5, **kwargs):
+	ax.scatter(x, y, marker=marker, alpha=alpha, **kwargs)
+
+def polyscatter_errorbar(ax, x, y, xerr=None, yerr=None, marker="o", alpha=1, **kwargs):
+	ax.errorbar(x, y, xerr=xerr, yerr=yerr, marker=marker, alpha=alpha, **kwargs)
+
+
--- yacd.py
+++ yacd.py
@@ -7,12 +7,14 @@ from matplotlib.figure import Figure
 import numpy as np
 import pandas as pd
 import plottingtools as pt
+
 pt.darkmode()
 
 import shutil
 import requests
 
 app = Flask(__name__)
+
 
 def get_strain_data():
     # Grab data. Using a stream because direct download with pandas yields 403 Forbidden
@@ -51,10 +53,14 @@ def get_strain_data():
         else:
             colnames_new.append("Other")
     data.columns = colnames_new
-    
+
     return data
 
-def get_vaccination_data(url: str = "https://impfdashboard.de/static/data/germany_vaccinations_timeseries_v2.tsv") -> pd.DataFrame:
+
+def get_vaccination_data(
+    url:
+    str = "https://impfdashboard.de/static/data/germany_vaccinations_timeseries_v2.tsv"
+) -> pd.DataFrame:
     # Read CSV table from web
     data = pd.read_table(url)
 
@@ -71,30 +77,37 @@ def get_vaccination_data(url: str = "https://impfdashboard.de/static/data/german
 
     return data
 
-def get_case_data(url: str = "https://raw.githubusercontent.com/robert-koch-institut/SARS-CoV-2-Nowcasting_und_-R-Schaetzung/main/Nowcast_R_aktuell.csv") -> pd.DataFrame:
+
+def get_case_data(
+    url:
+    str = "https://raw.githubusercontent.com/robert-koch-institut/SARS-CoV-2-Nowcasting_und_-R-Schaetzung/main/Nowcast_R_aktuell.csv"
+) -> pd.DataFrame:
     # Read CSV table from web
     data = pd.read_table(url, sep=",")
 
     # Convert date column to datetime data type for better plotting later
     data['Datum'] = pd.to_datetime(data['Datum'])
-    
-    # Rolling mean
-    data["cases_rollingmean"] = data.rolling(window=7)["PS_COVID_Faelle"].mean()
 
+    # Rolling mean
+    data["cases_rollingmean"] = data.rolling(
+        window=7)["PS_COVID_Faelle"].mean()
 
     return data
+
 
 def get_bed_data(url: str = "") -> pd.DataFrame:
     # Read CSV table from web
     data=pd.read_table('https://diviexchange.blob.core.windows.net/%24web/zeitreihe-tagesdaten.csv',\
                   sep=",")
-    
+
     # Convert date column to datetime data type for better plotting later
     data['date'] = pd.to_datetime(data['date'])
-    
+
     # Aggregate per day
-    data = data.drop(columns=['bundesland', 'gemeindeschluessel', 'anzahl_standorte',
-        'anzahl_meldebereiche'])
+    data = data.drop(columns=[
+        'bundesland', 'gemeindeschluessel', 'anzahl_standorte',
+        'anzahl_meldebereiche'
+    ])
     data = data.groupby("date").sum()
     data['date'] = data.index
 
@@ -102,6 +115,7 @@ def get_bed_data(url: str = "") -> pd.DataFrame:
     data["betten_sum"] = data["betten_frei"] + data["betten_belegt"]
 
     return data
+
 
 def make_vac_plot_cumul(data: pd.DataFrame) -> str:
     # Generate the figure **without using pyplot**.
@@ -122,13 +136,14 @@ def make_vac_plot_cumul(data: pd.DataFrame) -> str:
     pt.limits(ax, None, (0, 1e8))
     fig.set_figheight(10)
     fig.set_figwidth(15)
-    
+
     # Save it to a temporary buffer.
     buf = BytesIO()
     fig.savefig(buf, format="png")
 
     # Convert to ASCII Base64
     return base64.b64encode(buf.getbuffer()).decode("ascii")
+
 
 def make_vac_plot_daily(data: pd.DataFrame) -> str:
     # Generate the figure **without using pyplot**.
@@ -136,14 +151,13 @@ def make_vac_plot_daily(data: pd.DataFrame) -> str:
     ax = fig.subplots()
 
     # Plot
-    pt.polyscatter(ax,data.date, data.erst_daily)
-    pt.polyscatter(ax,data.date, data.zweit_daily)
-    pt.polyscatter(ax,data.date, data.dritt_daily)
+    pt.polyscatter(ax, data.date, data.erst_daily)
+    pt.polyscatter(ax, data.date, data.zweit_daily)
+    pt.polyscatter(ax, data.date, data.dritt_daily)
 
-    pt.majorline(ax,data.date, data.erst_rollingmean, label="First")
-    pt.majorline(ax,data.date, data.zweit_rollingmean, label="Second")
-    pt.majorline(ax,data.date, data.dritt_rollingmean, label="Third")
-
+    pt.majorline(ax, data.date, data.erst_rollingmean, label="First")
+    pt.majorline(ax, data.date, data.zweit_rollingmean, label="Second")
+    pt.majorline(ax, data.date, data.dritt_rollingmean, label="Third")
 
     # Aesthetics
     fig.autofmt_xdate(bottom=0.2, rotation=40, ha='right')
@@ -151,18 +165,23 @@ def make_vac_plot_daily(data: pd.DataFrame) -> str:
     pt.ticklabelsize(ax)
     pt.legend(ax)
     pt.labels(ax, "Date", "Daily Vaccinations")
-    ymax = np.max([np.max(data.erst_daily), np.max(data.zweit_daily), np.max(data.dritt_daily)])
-    ymax = round(ymax, -6)+1000000
-    pt.limits(ax, None, (0,ymax))
+    ymax = np.max([
+        np.max(data.erst_daily),
+        np.max(data.zweit_daily),
+        np.max(data.dritt_daily)
+    ])
+    ymax = round(ymax, -6) + 1000000
+    pt.limits(ax, None, (0, ymax))
     fig.set_figheight(10)
     fig.set_figwidth(15)
-    
+
     # Save it to a temporary buffer.
     buf = BytesIO()
     fig.savefig(buf, format="png")
 
     # Convert to ASCII Base64
     return base64.b64encode(buf.getbuffer()).decode("ascii")
+
 
 def make_case_plot(data: pd.DataFrame) -> str:
     # Generate the figure **without using pyplot**.
@@ -173,24 +192,23 @@ def make_case_plot(data: pd.DataFrame) -> str:
     pt.polyscatter(ax, data.Datum, data.PS_COVID_Faelle)
     pt.majorline(ax, data.Datum, data.cases_rollingmean)
 
-
     # Aesthetics
     fig.autofmt_xdate(bottom=0.2, rotation=40, ha='right')
     pt.despine(ax)
     pt.ticklabelsize(ax)
-    pt.legend(ax)
     pt.labels(ax, "Date", "Daily Cases")
-    ymax = round(max(data.PS_COVID_Faelle), -4)+10000
+    ymax = round(max(data.PS_COVID_Faelle), -4) + 10000
     pt.limits(ax, None, (0, ymax))
     fig.set_figheight(10)
     fig.set_figwidth(15)
-    
+
     # Save it to a temporary buffer.
     buf = BytesIO()
     fig.savefig(buf, format="png")
 
     # Convert to ASCII Base64
-    return base64.b64encode(buf.getbuffer()).decode("ascii")  
+    return base64.b64encode(buf.getbuffer()).decode("ascii")
+
 
 def make_bed_plot(data: pd.DataFrame) -> str:
     # Generate the figure **without using pyplot**.
@@ -206,19 +224,25 @@ def make_bed_plot(data: pd.DataFrame) -> str:
     fig.autofmt_xdate(bottom=0.2, rotation=40, ha='right')
     pt.despine(ax)
     pt.ticklabelsize(ax)
-    pt.legend(ax)
     pt.labels(ax, "Date", "Beds")
-    ymax = round(np.max([np.max(data.betten_frei), np.max(data.betten_belegt), np.max(data.betten_sum)]), -4)+10000
+    ymax = round(
+        np.max([
+            np.max(data.betten_frei),
+            np.max(data.betten_belegt),
+            np.max(data.betten_sum)
+        ]), -4) + 10000
     pt.limits(ax, None, (0, ymax))
+    pt.legend(ax)
     fig.set_figheight(10)
     fig.set_figwidth(15)
-    
+
     # Save it to a temporary buffer.
     buf = BytesIO()
     fig.savefig(buf, format="png")
 
     # Convert to ASCII Base64
-    return base64.b64encode(buf.getbuffer()).decode("ascii")  
+    return base64.b64encode(buf.getbuffer()).decode("ascii")
+
 
 def make_strain_plot(data: pd.DataFrame) -> str:
     # Generate the figure **without using pyplot**.
@@ -238,30 +262,30 @@ def make_strain_plot(data: pd.DataFrame) -> str:
     pt.limits(ax, None, (0, 100.3))
     fig.set_figheight(10)
     fig.set_figwidth(15)
-    
+
     # Save it to a temporary buffer.
     buf = BytesIO()
     fig.savefig(buf, format="png")
 
     # Convert to ASCII Base64
-    return base64.b64encode(buf.getbuffer()).decode("ascii")  
-   
+    return base64.b64encode(buf.getbuffer()).decode("ascii")
+
+
 @app.route("/")
 def main():
     vac_data = get_vaccination_data()
     vacplot_cumul = make_vac_plot_cumul(vac_data)
     vacplot_daily = make_vac_plot_daily(vac_data)
-    
+
     case_data = get_case_data()
     case_plot = make_case_plot(case_data)
-    
+
     bed_data = get_bed_data()
     bed_plot = make_bed_plot(bed_data)
-    
+
     strain_data = get_strain_data()
     strain_plot = make_strain_plot(strain_data)
-    
-    
+
     return f'''<html><head><link rel="stylesheet" href="/static/style.css"></head><body>
     <h1>Yet another Covid Dashboard!</h1>
     <h2>Cases (data polled from RKI github and RKI website)</h2>
